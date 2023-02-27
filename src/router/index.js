aa1728d670a72242ccccfeb277ab89cfaaf203ab
Ohloule <louislayani2@gmail.com>
2023-02-27 11:04:31 +0100

    fsdf

--- src/router/index.js
+++ src/router/index.js
@@ -49,7 +49,7 @@ const routes = [
 
 const router = createRouter({
   mode: 'history',
-  history: createWebHistory(process.env.BASE_URL),
+//  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
